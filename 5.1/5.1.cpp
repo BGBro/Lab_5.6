1a0105a502f9c984f4ead63514bf7622bf40b20f
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2020-11-05 11:21:42 +0200

    master

---
+++ 5.1/5.1.cpp
@@ -0,0 +1,23 @@
+﻿// Lab 5_1
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double k(const double x, const double y); // прототип
+
+int main()
+{
+	double p, q;
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+	double c = (k(1 + pow(p, 2), 1 - pow(q, 2)) - pow(k(1, p * q), 2)) / (1 + k(p * q, 1));
+	cout << "c = " << c << endl;
+	return 0;
+}
+
+double k(const double x, const double y) // визначення
+{
+	return ((sin(x) / (pow(x, 2) + pow(y, 2))) + (cos(y) / (1 + abs(x * y))));
+}
